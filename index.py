bf2d656ff6c7a036fc98fdb6ffdb15d5719a1484
benhexie <benedictgabriel73@gmail.com>
2023-11-18 20:21:07 +0100

    Added remaining requests and their functionalities

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import os
 import secrets
+from bson import ObjectId
 from flask import Flask, request, render_template, flash, redirect, url_for, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -29,8 +31,9 @@ except Exception as e:
     raise e
 
 
+@app.route('/blog/<string:blog_id>', methods=['GET'])
 @app.route('/blogs/new-blog', methods=['POST', 'GET'])
-def add_blog():
+def add_blog(blog_id=""):
     """The Add New Blog Route"""
     if request.method == 'POST':
         try:
@@ -40,25 +43,30 @@ def add_blog():
 
             new_blog = {
                 'title': blogTitle,
-                'body': blogBody
+                'body': blogBody,
+                'createdAt': datetime.now()
             }
-
             my_blog.insert_one(new_blog)
             flash('Blog uploaded to the database', 'success')
-            return redirect(url_for('get_blogs'))
+            return redirect(url_for('get_blogs_page'))
         except Exception as e:
             print(f"Error adding blog: {e}")
             flash(f"Error adding blog: {e}", 'danger')
             return jsonify({'error': f"Error: {e}"}), 500
-
-    return render_template('new-blog.html')
+        
+    blog = {}
+    try:
+        blog = my_blog.find_one({'_id': ObjectId(blog_id)})
+        blog['_id'] = str(blog['_id'])
+    except Exception as e:
+        print(f"Error retrieving blog: {e}")
+    return render_template('new-blog.html', blog=blog)
 
 
 
 
 @app.route('/', methods=['GET'])
-@app.route('/blogs/', methods=['GET'])
-def get_blogs():
+def get_blogs_page():
     """The Get Blogs Route"""
     try:
         blogs = list(my_blog.find())
@@ -67,13 +75,55 @@ def get_blogs():
             print(blog)
     except Exception as e:
         print(f"Error retrieving blogs: {e}")
-    return render_template('blogs.html', blogs=blogs)
+    return render_template('blogs.html')
 
+@app.route('/blogs', methods=['GET'])
+def get_blogs():
+    """Get the blogs from the database"""
+    try:
+        blogsRaw = my_blog.find()
+        blogs = []
+        for blog in blogsRaw:
+            blog['_id'] = str(blog['_id'])
+            blogs.append(blog)
+        return jsonify(blogs), 200
+    except Exception as e:
+        print(f"Error retrieving blogs: {e}")
+        return jsonify({'error': f"Error: {e}"}), 500
+    
+@app.route('/blog/<string:blog_id>', methods=['DELETE'])
+def delete_blog(blog_id):
+    """Delete a blog from the database"""
+    try:
+        my_blog.delete_one({'_id': ObjectId(blog_id)})
+        return jsonify({'message': 'Blog deleted successfully'}), 200
+    except Exception as e:
+        print(f"Error deleting blog: {e}")
+        return jsonify({'error': f"Error: {e}"}), 500
+    
+@app.route('/blog/<string:blog_id>', methods=['PUT'])
+def update_blog(blog_id):
+    """Update a blog in the database"""
+    try:
+        data = request.get_json()
+        blogTitle = data.get('blogTitle')
+        blogBody = data.get('blogBody')
+
+        my_blog.update_one({'_id': ObjectId(blog_id)}, {'$set': {'title': blogTitle, 'body': blogBody}})
+        return jsonify({'message': 'Blog updated successfully'}), 200
+    except Exception as e:
+        print(f"Error updating blog: {e}")
+        return jsonify({'error': f"Error: {e}"}), 500
 
 @app.route('/support')
 def support_page():
     """The support route"""
     return render_template('support.html')
+
+@app.route('/fake-support')
+def fake_support_page():
+    """The fake-support route"""
+    return render_template('fake-support.html')
 
 
 
